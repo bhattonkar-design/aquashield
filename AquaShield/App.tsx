0a4ed70f809bbf75920de7d1a874e69e1de0c971
Onkar Bhatt <bhattonkar6@gmail.com>
2026-09-04 23:56:14 +0530

    fix: replace CARTO basemap with keyless dark-filtered OpenStreetMap tiles

--- AquaShield/App.tsx
+++ AquaShield/App.tsx
@@ -84,8 +84,9 @@ const InteractiveMap: React.FC<InteractiveMapProps> = ({
         attributionControl: false,
       }).setView([lat, lon], 13);
 
-      L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
+        className: 'dark-tiles',
       }).addTo(map);
 
       L.control.zoom({ position: 'bottomright' }).addTo(map);
